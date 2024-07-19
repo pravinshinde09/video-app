15c7cc31ee05fc8ba0603f70bd7881bb434fcc5e
Gajanan-bhosale <gajananbhosale.official@gmail.com>
2024-07-19 14:52:44 +0530

    modified 2:52

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,19 @@
 import React, { useRef, useState } from "react";
 import Sidebar from "./Slider";
 import { Button } from "@mui/material";
+import {Stack, Avatar } from '@mui/material'
 import ReactPlayer from "react-player";
 import ContentCutIcon from "@mui/icons-material/ContentCut";
 import VideocamIcon from '@mui/icons-material/Videocam';
 import PodcastsIcon from '@mui/icons-material/Podcasts';
 import MicNoneIcon from '@mui/icons-material/MicNone';
 import BoltOutlinedIcon from '@mui/icons-material/BoltOutlined';
+import { BorderColor } from "@mui/icons-material";
 
 const Home = ({ mainVideoUrl, frameVideoUrl }) => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const videoRef = useRef(null);
-  const frameRef = useRef(null);
-  const handleScroll = (e) => {
-    const { scrollLeft, clientWidth, scrollWidth } = e.target;
-    const maxScrollLeft = scrollWidth - clientWidth;
 
-    if (scrollLeft >= maxScrollLeft) {
-      e.target.scrollLeft = 0;
-    }
-  };
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
@@ -48,11 +42,31 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
       padding: "10px",
       fontSize: "16px",
       margin: "10px 0",
-      width: "100%",
+      marginTop:'20px',
+      width: "200%",
+      innerHeight: '50%',
+      BorderColor:'none',
+      border :'1px',
       boxSizing: "border-box",
       borderRadius: "10px",
+      boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      transition: "box-shadow 0.3s ease",
     },
-    TopButtonStyle: {
+    TopButtonStyle1: {
+      border: "1px",
+      paddingInline: "20px",
+      padding: '10px',
+      marginInline: '10px',
+      marginTop: '40px',
+      color:'black',
+      borderRadius: "10px",
+      backgroundColor: '#fec76f',
+      boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      transition: "box-shadow 0.3s ease",
+      marginRight:'70px',
+
+    },
+    TopButtonStyle2: {
       border: "1px",
       paddingInline: "20px",
       padding: '10px',
@@ -60,13 +74,17 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
       marginTop: '30px',
       borderRadius: "10px",
       backgroundColor: 'white',
+      color: 'black',
       boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
       transition: "box-shadow 0.3s ease",
+      
+      
 
     },
     buttonStyle: {
       border: "1px",
       paddingInline: "20px",
+      width:'30%',
       borderRadius: "10px",
       backgroundColor: 'white',
       boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
@@ -75,15 +93,15 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
     },
 
     videoPlayerContainer: {
-      width: '40%',
+      width: '20%',
       padding: "10px",
-      borderRadius: "10%",
+      opacity: "30px",
 
     },
 
     avatar: {
-      width: '50px',
-      height: '50px',
+      width: '70px',
+      height: '70px',
       borderRadius: '50%',
       marginRight: '10px',
     },
@@ -91,12 +109,14 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
+      
     },
     
     mainVideo: {
       width: '80%',
       height: 'auto',
       marginBottom: '20px',
+      
     },
     
     frameContainer: {
@@ -132,17 +152,22 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
             />
           </div>
 
-          <div style={{ position: 'absolute', right: '0', top: '0' }}>
-            <button startIcon={<BoltOutlinedIcon />} style={styles.TopButtonStyle} >Upgrade</button>
-            <button startIcon={<BoltOutlinedIcon />} style={styles.TopButtonStyle} >Invite</button>
-            <img src="avatar.jpg" alt="Avatar" style={styles.avatar} />
+          <div style={{ position: 'absolute', right: '0', top: '0', flexDirection:'row' }}>
+            <Button startIcon={<BoltOutlinedIcon />} style={styles.TopButtonStyle1} >Upgrade</Button>
+            <Button startIcon={<BoltOutlinedIcon />} style={styles.TopButtonStyle2} >Invite</Button>
+            {/* <img src="avatar.jpg" alt="Avatar" style={styles.avatar} /> */}
+            <Stack spacing={3}>
+              <stack direction='row' spacing={1}>
+                <Avatar>BM</Avatar>
+              </stack>
+            </Stack>
           </div>
 
         </div>
-        <p style={{ fontSize: "24px", marginTop: '60px', marginLeft: '20px' }}>
+        <p style={{ fontSize: "24px", marginTop: '40px', marginLeft: '20px' }}>
           Let's Create some <span style={{ fontWeight: "bold" }}>Videos!</span>{" "}
         </p>
-        <div style={{ display: "flex", padding: "20px", gap: "60px", flexDirection: 'row' }}>
+        <div style={{ display: "flex", padding: "20px", gap: "50px", flexDirection: 'row' , marginTop:'-15px'}}>
           <div style={styles.buttonStyle}>
             <Button startIcon={<ContentCutIcon />} style={{ color: 'purple' }}>
               <p style={{ fontSize: '15px', fontWeight: '10px', color: 'black' }}>Create Project</p>
@@ -164,7 +189,7 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
             </Button>
           </div>
         </div>
-        <div style={{ marginLeft: '10px', fontSize: '25px' }}>
+        <div style={{ marginLeft: '10px', fontSize: '25px' ,marginBottom:'-20px'}}>
           <p >My Recent Videos</p>
         </div>
         <div style={styles.videoPlayerContainer}>
@@ -174,12 +199,7 @@ const Home = ({ mainVideoUrl, frameVideoUrl }) => {
             ref={videoRef}
           />
         </div>
-        <div className="videoContainer">
-          <ReactPlayer url={mainVideoUrl} playing controls className="mainVideo" />
-          <div className="frameContainer" onScroll={handleScroll} ref={frameRef}>
-            <ReactPlayer url={frameVideoUrl} playing muted className="frameVideo" />
-          </div>
-        </div>
+        
       </div>
     </div>
   );
