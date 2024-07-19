db5bbcb8284f15ca5ea577ee7e86fa3257a76a30
Gajanan-bhosale <gajananbhosale.official@gmail.com>
2024-07-19 10:47:01 +0530

    Updated File

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useRef,useState } from "react";
 import Sidebar from "./Slider";
+import { Button } from "@mui/material";
+import ReactPlayer from "react-player";
+import ContentCutIcon from "@mui/icons-material/ContentCut";
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import MicNoneIcon from '@mui/icons-material/MicNone';
+import BoltOutlinedIcon from '@mui/icons-material/BoltOutlined';
 
 const Home = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
-
+  const videoRef = useRef(null);
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
@@ -11,7 +18,7 @@ const Home = () => {
   const styles = {
     container: {
       display: "flex",
-      backgroundColor: "#DADADA",
+      backgroundColor: "#f7f7f9",
     },
     mainContent: {
       marginLeft: isSidebarOpen ? "10px" : "0",
@@ -20,7 +27,7 @@ const Home = () => {
       width: "100%",
     },
     toggleButton: {
-      top: "10px",
+      top: "5px",
       left: isSidebarOpen ? "260px" : "10px",
       zIndex: "1000",
       color: "black",
@@ -30,35 +37,46 @@ const Home = () => {
     searchBar: {
       padding: "10px",
       fontSize: "16px",
-      margin: "20px 0",
+      margin: "10px 0",
       width: "100%",
       boxSizing: "border-box",
       borderRadius: "10px",
     },
     TopButtonStyle: {
-        border: "1px",
-        paddingInline: "20px",
-        padding:'10px',
-        marginInline:'10px',
-        borderRadius: "10px",
-        backgroundColor: 'white',
-        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-        transition: "box-shadow 0.3s ease", 
+      border: "1px",
+      paddingInline: "20px",
+      padding: '10px',
+      marginInline: '10px',
+      marginTop: '30px',
+      borderRadius: "10px",
+      backgroundColor: 'white',
+      boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      transition: "box-shadow 0.3s ease",
+
     },
     buttonStyle: {
-        border: "1px",
-        paddingInline: "20px",
-        borderRadius: "10px",
-        backgroundColor: 'white',
-        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-        transition: "box-shadow 0.3s ease", 
+      border: "1px",
+      paddingInline: "20px",
+      borderRadius: "10px",
+      backgroundColor: 'white',
+      boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      transition: "box-shadow 0.3s ease",
+      fontWeight: 'bold',
     },
+
+    videoPlayerContainer: {
+      width:'40%',
+      padding: "10px",
+      borderRadius: "10%",
+      
+    },
+
     avatar: {
-        width: '50px',
-        height: '50px',
-        borderRadius: '50%',
-        marginRight: '10px',
-      },
+      width: '50px',
+      height: '50px',
+      borderRadius: '50%',
+      marginRight: '10px',
+    },
   };
 
   return (
@@ -78,32 +96,47 @@ const Home = () => {
             />
           </div>
 
-          <div style={{position:'absolute', right:'0',top:'0'}}>
-          <button style={styles.TopButtonStyle}>Upgrade</button>
-          <button style={styles.TopButtonStyle}>Invite</button>
-          <img src="avatar.jpg" alt="Avatar" style={styles.avatar} />
+          <div style={{ position: 'absolute', right: '0',top: '0' }}>
+            <button startIcon={<BoltOutlinedIcon/>} style={styles.TopButtonStyle} >Upgrade</button>
+            <button startIcon={<BoltOutlinedIcon/>} style={styles.TopButtonStyle} >Invite</button>
+            <img src="avatar.jpg" alt="Avatar" style={styles.avatar} />
           </div>
 
         </div>
-        <p style={{ fontSize: "24px" }}>
+        <p style={{ fontSize: "24px", marginTop: '60px', marginLeft: '20px' }}>
           Let's Create some <span style={{ fontWeight: "bold" }}>Videos!</span>{" "}
         </p>
-        <div style={{ display: "flex", padding: "20px", gap: "40px" }}>
+        <div style={{ display: "flex", padding: "20px", gap: "60px", flexDirection: 'row' }}>
           <div style={styles.buttonStyle}>
-            <p>Create Project</p>
+            <Button startIcon={<ContentCutIcon />} style={{color:'purple'}}>
+              <p style={{ fontSize: '15px', fontWeight: '10px', color: 'black' }}>Create Project</p>
+            </Button>
           </div>
           <div style={styles.buttonStyle}>
-            <p>Record Video</p>
+            <Button startIcon={<VideocamIcon />} style={{color:'red'}} >
+              <p style={{ fontSize: '15px', fontWeight: '10px', color: 'black' }}>Record Video</p>
+            </Button>
           </div>
           <div style={styles.buttonStyle}>
-            <p>Go Live</p>
+            <Button startIcon={<PodcastsIcon />} style={{color:'green'}} >
+              <p style={{ fontSize: '15px', fontWeight: '10px', color: 'black' }}>Go Live</p>
+            </Button>
           </div>
           <div style={styles.buttonStyle}>
-            <p>Record Podcast</p>
+            <Button startIcon={<MicNoneIcon />} style={{color:'yellow'}}>
+              <p style={{ fontSize: '15px', fontWeight: '10px', color: 'black' }}>Record Podcast</p>
+            </Button>
           </div>
         </div>
-        <div>
-            <p>My Recent Videos</p>
+        <div style={{marginLeft: '10px',fontSize: '25px'}}>
+          <p >My Recent Videos</p>
+        </div>
+        <div style={styles.videoPlayerContainer}>
+          <ReactPlayer
+            url="https://www.w3schools.com/html/mov_bbb.mp4"
+            controls
+            ref={videoRef}
+          />
         </div>
       </div>
     </div>
